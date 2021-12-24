7f8851bf6180f1e5c2d224f58be7bb53da3a2e36
viha191tran <96633746+viha191tran@users.noreply.github.com>
2021-12-25 03:12:46 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,16 +1,16 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
-  text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text1: "Hi Thầy!",
+  text2: "Em có điều này muốn hỏi Thầy nhớ phải trả lời thật lòng nhaaa.",
+  text3: "Thầy có nhớ em hem? ._.",
+  text4: "Nếu Thầy hem trả lời mà thoát ra là Thầy hông nhận được quà của em đâu đó :>",
+  text5: "Bình thường :)))",
+  text6: "Nhớ lắm lun :(((",
+  text7: "Thầy nhớ em nhìu hem? :>",
+  text8: "Gửi cho em <3",
+  text9: "Nhiều lém lém :((",
+  text10: "Em biết mà ^^ Em cũng nhớ Thầy nhìu :(",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
+    "Qua Tết vào SG lại Thầy dẫn em đi ăn với đi chụp hình nha :>",
   text12: "Okii lunn <3",
 };
 
