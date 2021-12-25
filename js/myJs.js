f3c8b9c3ba243456205ed2438685a6be235a049b
viha191tran <96633746+viha191tran@users.noreply.github.com>
2021-12-25 07:12:46 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -114,7 +114,7 @@ $(document).ready(function () {
       html: true,
       width: 900,
       padding: "3em",
-      html: "<input type='text' class='form-control' id='txtReason'  placeholder='...'>",
+      html: "<input type='text' class='form-control' id='txtReason'  placeholder='Cóa nhìu lắmmm <3'>",
       background: '#fff url("img/iput-bg.jpg")',
       backdrop: `
                     rgba(0,0,123,0.4)
